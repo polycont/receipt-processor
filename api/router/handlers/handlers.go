1cafb1051f377f99333ceeca1a18df47f1a592cd
Cohen <22184868+polycont@users.noreply.github.com>
2024-11-28 11:35:36 -0600

    Remove unnecessary comments, split code into functions

--- api/router/handlers/handlers.go
+++ api/router/handlers/handlers.go
@@ -21,7 +21,6 @@ type Item struct {
 	Price            string `json:"price"`
 }
 
-// Struct defining the receipt as a whole
 type Receipt struct {
 	Retailer     string `json:"retailer"`
 	PurchaseDate string `json:"purchaseDate"`
@@ -48,17 +47,15 @@ func ProcessReceipts(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Associate generated UUID with processed receipt
 	receiptId := uuid.New().String()
 	receipt.ReceiptId = receiptId
 
 	receipts = append(receipts, receipt)
-	receiptIdJson, err := json.Marshal(map[string]string{ "id": receiptId })
+	receiptIdJson, err := json.Marshal(map[string]string{"id": receiptId})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	// json.Marshal(receiptId)
 	w.Write([]byte(receiptIdJson))
 }
 
@@ -76,80 +73,63 @@ func CalculatePoints(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "No receipt ID available.", http.StatusBadRequest)
 	}
 
-	// Loop through receipts array to find a matching receipt based on receiptId
 	for _, receipt := range receipts {
-		// Validate that receiptId exists on the receipt
-		if receipt.ReceiptId != "" {
-			if receipt.ReceiptId == receiptId {
-				// Calculate points by rule
-				points += getAlphanumPoints(receipt.Retailer)
-				// Check if dollar amount is round by seeing if it matches truncated value
-				floatTotal, err := strconv.ParseFloat(receipt.Total, 64)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-					return
-				}
-				if floatTotal == math.Trunc(floatTotal) {
-					points += 50
-				}
-				// Check if total is a multiple of 0.25
-				if math.Mod(floatTotal, 0.25) == 0 {
-					points += 25
-				}
-				// Award 5 points for every 2 items on the receipt
-				itemCount := len(receipt.Items)
-				points += (itemCount / 2) * 5
-				// Award points based on trimmed description length
-				items := receipt.Items
-				for _, item := range items {
+		if receipt.ReceiptId != "" && receipt.ReceiptId == receiptId {
+			// Increment points based on the # of alphanumeric characters in the retailer name
+			points += getAlphanumPoints(receipt.Retailer)
 
-					// Trim description of all whitespace
-					description := item.ShortDescription
-					description = strings.TrimSpace(description)
-
-					// Getting length of trimmed description
-					descriptionLength := len(strings.TrimSpace(description))
-					// Check if description length is a multiple of 3
-					if math.Mod(float64(descriptionLength), 3) == 0 {
-						// Convert price to float
-						priceFloat, err := strconv.ParseFloat(item.Price, 64)
-						if err != nil {
-							http.Error(w, err.Error(), http.StatusBadRequest)
-							return
-						}
-						// Calculate rounded and converted points value
-						points += int(math.Ceil(priceFloat * 0.2))
-					}
-				}
-
-				// Award points if purchase day is odd
-				purchaseDate := receipt.PurchaseDate
-				purchaseDateArray := strings.Split(purchaseDate, "-")
-				purchaseDay, err := strconv.ParseFloat(purchaseDateArray[2], 64)
-				if math.Mod(float64(purchaseDay), 2.0) != 0 {
-					points += 6
-				}
-
-				// Award points if purchase time is between 2pm and 4pm
-				purchaseTime := receipt.PurchaseTime
-				purchaseTimeArray := strings.Split(purchaseTime, ":")
-				purchaseTimeInt, err := strconv.Atoi(strings.Join(purchaseTimeArray, ""))
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-					return
-				}
-
-				if purchaseTimeInt > 1400 && purchaseTimeInt < 1600 {
-					points += 10
-				}
-
+			floatTotal, err := strconv.ParseFloat(receipt.Total, 64)
+			if err != nil {
+				http.Error(w, "Error converting total to float.", http.StatusBadRequest)
+				return
 			}
+
+			// Check if total is a whole number; if so, award 50 points
+			points += getRoundPoints(floatTotal)
+
+			if math.Mod(floatTotal, 0.25) == 0 {
+				points += 25
+			}
+
+			itemCount := len(receipt.Items)
+			points += (itemCount / 2) * 5
+
+			// Award points based on trimmed description length of items
+			items := receipt.Items
+			descPoints, err := getDescriptionPoints(items)
+			if err != nil {
+				http.Error(w, "Error calculating description length points.", http.StatusBadRequest)
+				return
+			}
+			points += descPoints
+
+			// Award points if purchase day is odd
+			purchaseDate := receipt.PurchaseDate
+			purchaseDateArray := strings.Split(purchaseDate, "-")
+			purchaseDay, err := strconv.ParseFloat(purchaseDateArray[2], 64)
+			if math.Mod(float64(purchaseDay), 2.0) != 0 {
+				points += 6
+			}
+
+			// Award points if purchase time is between 2pm and 4pm
+			purchaseTime := receipt.PurchaseTime
+			purchaseTimeArray := strings.Split(purchaseTime, ":")
+			purchaseTimeInt, err := strconv.Atoi(strings.Join(purchaseTimeArray, ""))
+			if err != nil {
+				http.Error(w, "Error converting purchase time to int.", http.StatusBadRequest)
+				return
+			}
+
+			if purchaseTimeInt > 1400 && purchaseTimeInt < 1600 {
+				points += 10
+			}
+
 		}
 
 	}
-	pointsJson, err := json.Marshal(map[string]int{ "points": points })
+	pointsJson, err := json.Marshal(map[string]int{"points": points})
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		http.Error(w, "Error marshalling points into JSON.", http.StatusBadRequest)
 		return
 	}
 
@@ -162,12 +142,35 @@ func getAlphanumPoints(retailerName string) int {
 	regex := regexp.MustCompile("^[a-zA-Z0-9]*$")
 	alphaNumPoints := 0
 
-	// Loop through provided string as an array
 	for _, c := range retailerName {
 		if regex.MatchString(string(c)) {
 			alphaNumPoints += 1
 		}
 	}
-
 	return alphaNumPoints
+}
+
+func getRoundPoints(total float64) int {
+	if total == math.Trunc(total) {
+		return 50
+	}
+	return 0
+}
+
+func getDescriptionPoints(items []Item) (int, error) {
+	pointsToReturn := 0
+	for _, item := range items {
+
+		description := item.ShortDescription
+		descriptionLength := len(strings.TrimSpace(description))
+
+		if math.Mod(float64(descriptionLength), 3) == 0 {
+			priceFloat, err := strconv.ParseFloat(item.Price, 64)
+			if err != nil {
+				return 0, err
+			}
+			pointsToReturn += int(math.Ceil(priceFloat * 0.2))
+		}
+	}
+	return pointsToReturn, nil
 }
